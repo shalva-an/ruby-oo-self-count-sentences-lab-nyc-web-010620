4bc812363ab864b850f02949afa80ceb93246fd6
shalva-an <s.halvaa26@gmail.com>
2020-01-08 18:05:26 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # https://stackoverflow.com/questions/26146558/using-more-than-one-delimiter-for-split-method-in-ruby
+    # Don't count "!!" or ".." as multiple sentences
+    # https://apidock.com/ruby/Enumerable/reject
+    self.split(/[.!?]/).reject {|x| x.size < 2}.count
   end
 end
